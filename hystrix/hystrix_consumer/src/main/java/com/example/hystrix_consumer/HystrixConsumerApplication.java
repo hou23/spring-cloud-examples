e3f4cddf68349658894313fab3cd2eeaec72e3b1
yaojie.hou <yaojie.hou@ilaw66.com>
2018-08-09 11:27:18 +0800

    引入hystrix dashboard, 并设置管理端点

--- hystrix/hystrix_consumer/src/main/java/com/example/hystrix_consumer/HystrixConsumerApplication.java
+++ hystrix/hystrix_consumer/src/main/java/com/example/hystrix_consumer/HystrixConsumerApplication.java
@@ -3,11 +3,13 @@ package com.example.hystrix_consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class HystrixConsumerApplication {
 
 	public static void main(String[] args) {
