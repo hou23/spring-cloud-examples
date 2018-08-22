2dd01616694ddb5e6d68741c655f2e3acfaa7c8b
yaojie.hou <yaojie.hou@ilaw66.com>
2018-08-22 15:26:05 +0800

    测试切换不同环境配置文件

--- config/config-client/src/main/java/com/example/configclient/controller/HelloController.java
+++ config/config-client/src/main/java/com/example/configclient/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@Value("${dev.hello}")
+	@Value("${config.hello}")
 	private String hello;
 
 	@RequestMapping("/hello")
